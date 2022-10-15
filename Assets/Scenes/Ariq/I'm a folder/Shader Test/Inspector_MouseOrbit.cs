a012af20047bf07ceabcb9223ea02833a19b1544
AriqMusyaffa <ariqmusyaffa@yahoo.com>
2022-10-15 21:04:49 +0700

    Add : Environment & Shader Test

---
+++ Assets/Scenes/Ariq/I'm a folder/Shader Test/Inspector_MouseOrbit.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inspector_MouseOrbit : MonoBehaviour
+{
+    [Header("Main Settings")]
+    public Transform target;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+    public float distanceMin = .5f;
+    public float distanceMax = 15f;
+    private Rigidbody rigidbody;
+    float x = 0.0f;
+    float y = 0.0f;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        rigidbody = GetComponent<Rigidbody>();
+
+        // Make the rigidbody not change rotation
+        if (rigidbody != null)
+        {
+            rigidbody.freezeRotation = true;
+        }
+
+        Cursor.visible = false;
+    }
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position + new Vector3(0, 0.5f, 0);
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+        {
+            angle += 360F;
+        }
+        if (angle > 360F)
+        {
+            angle -= 360F;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
