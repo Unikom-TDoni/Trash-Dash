53576402b7bcde905293d5ac1db8db8ab4c83aa6
Brian Wijaya <70004754+briannzw@users.noreply.github.com>
2022-11-30 23:23:00 +0700

    Update Better Camera Obscure

--- Assets/Script/Feature/Camera/CameraObscure.cs
+++ Assets/Script/Feature/Camera/CameraObscure.cs
@@ -7,12 +7,14 @@ public class CameraObscure : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private LayerMask layerMask;
-    [SerializeField] private float radius = .2f;
+    [SerializeField, Range(0, 1)] private float radius = .2f;
 
     private Transform obstruction;
     private MeshRenderer mesh;
 
-    private Vector3[] origins;
+    private Vector3[] origins = new Vector3[5];
+
+    float maxDistance;
 
     private void Start()
     {
@@ -22,6 +24,7 @@ public class CameraObscure : MonoBehaviour
 
     private void SetOrigins()
     {
+        //origins = new Vector3[5];
         origins[0] = transform.position;
         origins[1] = transform.position + new Vector3(radius, 0f, 0f);
         origins[2] = transform.position - new Vector3(radius, 0f, 0f);
@@ -34,7 +37,8 @@ public class CameraObscure : MonoBehaviour
         SetOrigins();
         foreach (var origin in origins)
         {
-            if (Physics.Raycast(origin, target.position - transform.position, out var hit, (target.position - transform.position).sqrMagnitude, layerMask))
+            maxDistance = Mathf.Sqrt((origin - target.position).sqrMagnitude);
+            if (Physics.Raycast(origin, (target.position - transform.position).normalized, out var hit, maxDistance, layerMask))
             {
                 if (!hit.collider.CompareTag("Player"))
                 {
@@ -61,5 +65,14 @@ public class CameraObscure : MonoBehaviour
 
         if (!mesh) return;
         mesh.shadowCastingMode = mode;
+    }
+
+    private void OnDrawGizmos()
+    {
+        SetOrigins();
+        foreach (var origin in origins)
+        {
+            Gizmos.DrawRay(origin, (target.position - transform.position).normalized * Mathf.Sqrt((origin - target.position).sqrMagnitude));
+        }
     }
 }
