ed6f25cf686d144ebe112513d07667809585bceb
Brian Wijaya <70004754+briannzw@users.noreply.github.com>
2022-12-04 04:00:55 +0700

    Fix Butterfly Spawner Error dan After Game Cam FOV

--- Assets/Script/Feature/Spawner/ButterflySpawner.cs
+++ Assets/Script/Feature/Spawner/ButterflySpawner.cs
@@ -16,7 +16,7 @@ public class ButterflySpawner : MonoBehaviour
     {
         if (butterflyCount < maxSpawnLimit)
         {
-            int randomSpawner = Random.Range(0, maxSpawnLimit);
+            int randomSpawner = Random.Range(0, spawner.Length);
             var butterfly = Instantiate(butterflyPrefab, spawner[randomSpawner].transform.position, Quaternion.identity);
             var b = butterfly.GetComponent<ButterflyMovement>();
             b.butterflySpawner = this;
